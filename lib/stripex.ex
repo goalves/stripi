4d7d301f0b7f8785f96822413a1e8bfea2fdf92c
Gabriel Alves <g04lves@gmail.com>
2018-03-19 19:46:33 -0300

    Initial commit with empty project.

---
+++ lib/stripex.ex
@@ -0,0 +1,18 @@
+defmodule Stripex do
+  @moduledoc """
+  Documentation for Stripex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stripex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stripex_test.exs
@@ -0,0 +1,8 @@
+defmodule StripexTest do
+  use ExUnit.Case
+  doctest Stripex
+
+  test "greets the world" do
+    assert Stripex.hello() == :world
+  end
+end
