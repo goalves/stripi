17383960b761ff7a14fac9471feb32feedbcf756
Gabriel Alves <g04lves@gmail.com>
2019-03-08 13:58:26 -0300

    Accepts query parameters to customers endpoint.

--- lib/stripi/customers.ex
+++ lib/stripi/customers.ex
@@ -11,5 +11,5 @@ defmodule Stripi.Customers do
   def update(id, params, opts \\ []),
     do: Stripi.request(&post/3, [@url <> "/#{id}", params, opts])
 
-  def list(opts \\ []), do: Stripi.request(&get/2, [@url, opts])
+  def list(query \\ "", opts \\ []), do: Stripi.request(&get/2, [@url <> "?" <> query, opts])
 end
