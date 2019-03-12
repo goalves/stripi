7f3af097929dad9337ea7199e1d935c7409a7c0c
Gabriel Alves <g04lves@gmail.com>
2019-03-12 15:17:31 -0300

    [#1] Adds stripe token from circleci environment variable.

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,4 @@ use Mix.Config
 config :stripi, base_url: "https://api.stripe.com/v1"
 config :tesla, adapter: Tesla.Adapter.Hackney
 
-if File.exists?("config/secret.exs"), do: import_config("secret.exs")
+if File.exists?("config/test_secret.exs"), do: import_config("test_secret.exs")
